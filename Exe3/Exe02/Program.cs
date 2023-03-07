8d7054a2e7c8044ac822efb5fa8ea0d4bd0a9136
GustavoCZK <103081092+GustavoCZK@users.noreply.github.com>
2023-03-06 22:11:08 -0300

    Add files via upload

---
+++ Exe3/Exe02/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Olá, Seja Bem-Vindo!");
+Console.WriteLine("Olá, Qual é seu nome?");
+string name = Console.ReadLine();
+Console.WriteLine($"Olá {name} Praser em conhece-lo!");
+
+// Declarar sem innicializar / Primitivo
+
+string message1; 
+// Declarar inicializado / Abstrato
+String message2 = "Seu Nome Completo";
+
+// Declarar string vazia
+string message3 = String.Empty;
+string message4 = "";
+
+// Declaração com tipo implícito
+var message5 = "Mensagem Aleatória";
+
+message1 = "Nomenclatura;Pelé;The Rock;Roberto Carlos";
+var palavras = message1.Split(';');
+foreach(var word in palavras)
+{
+    // variavel de escopo local do laço de repetição foreach
+    int a = 10;
+    Console.WriteLine(word);
+}
