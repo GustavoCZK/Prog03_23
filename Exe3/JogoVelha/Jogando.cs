eb55c2015b5569772db714c20c10f955740604ac
Gustavo Conte <402497@unoesc.lan>
2023-03-27 22:01:53 -0300

    27/03/23 Third

---
+++ Exe3/JogoVelha/Jogando.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JogoVelha
+{
+    public class Jogando
+    {
+        public string Veia(bool var)
+        {
+            
+        }
+    }
+}
---
+++ Exe3/JogoVelha/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Digite X ou O: ");
+string entrada = Console.ReadLine();
+bool Ver = false;
+if(entrada == "X" | entrada == "O")
+{
+    Ver = true;
+}
+else
+{
+    Console.WriteLine("Nada Foi Encontrado, TENTE NOVAMENTE!");
+}
+
+string l00 = "";
+string l01 = "";
+string l02 = "";
+
+string l11 = "";
+string l12 = "";
+string l13 = "";
+
+string l21 = "";
+string l22 = "";
+string l23 = "";
+
+Console.WriteLine(l00);
