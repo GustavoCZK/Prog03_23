3302d41162774c4e866b559167fd40091b1d772a
Gustavo Conte <402497@unoesc.lan>
2023-04-17 22:06:47 -0300

    vai logo

---
+++ Exe3/Tabuada/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("------Tabuada------\n");
+
+Console.WriteLine("Digite o numero da tabuada: ");
+int baseN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Vezes(baseN);
+
+static void Vezes(int baseN)
+{   
+    int multiplier = 1;
+    while(multiplier <= 11)
+    {
+        Console.WriteLine($"{baseN} x {multiplier} = {baseN * multiplier}");
+        multiplier += 1;
+    }   
+}
+
